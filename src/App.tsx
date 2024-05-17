271e4a62a11bfdee18b2507651dde8ccd806df40
kangmin01 <minheek99@gmail.com>
2024-05-18 02:12:09 +0900

    lottie test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./App.css";
+import Lottie from "react-lottie";
+import ani from "./ahah.json";
 
 function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: ani,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <div className="App">
+      <Lottie options={defaultOptions} height={400} width={400} />
       <p className="bg-red-200 font-bold">haha</p>
     </div>
   );
