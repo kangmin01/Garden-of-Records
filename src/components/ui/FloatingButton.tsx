a016b67e7fd9c6bc1b82c6174dc2cf09a87db731
kangmin01 <minheek99@gmail.com>
2024-05-26 13:35:31 +0900

    fix: floating button positon

--- src/components/ui/FloatingButton.tsx
+++ src/components/ui/FloatingButton.tsx
@@ -16,15 +16,17 @@ export default function FloatingButton() {
     <>
       {ready && (
         <div
-          className={`w-[360px] max-w-[360px] mx-auto fixed`}
+          className={`w-full fixed h-10`}
           style={{ top: `${displayHeight - 1}px` }}
         >
-          <Link
-            to="/record/add"
-            className="absolute w-[52px] h-[52px] bottom-[34px] right-[30px] flex justify-center items-center bg-main text-white rounded-full shadow-shadowFloatingButton"
-          >
-            <PlusIcon />
-          </Link>
+          <div className="w-[360px] mx-auto relative">
+            <Link
+              to="/record/add"
+              className="absolute w-[52px] h-[52px] bottom-[34px] right-[30px] flex justify-center items-center bg-main text-white rounded-full shadow-shadowFloatingButton"
+            >
+              <PlusIcon />
+            </Link>
+          </div>
         </div>
       )}
     </>
