aaa68ee53ac82e7fdb20d8d2221e496bd84959e7
kangmin01 <minheek99@gmail.com>
2024-06-09 17:29:05 +0900

    feat: Create reusable button component

---
+++ src/components/ui/SubmitButton.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+type Props = {
+  navigate: boolean;
+  title: string;
+  link?: string;
+  textColor?: string;
+  bgColor?: string;
+  disabledColor?: string;
+};
+
+export default function SubmitButton({
+  navigate,
+  title,
+  link = "/",
+  textColor,
+  bgColor,
+  disabledColor,
+}: Props) {
+  return navigate ? (
+    <Link
+      to={link}
+      className={`${bgColor ? bgColor : "bg-gray1"} ${textColor ? textColor : "text-white"} w-[320px] h-[48px] font-semibold text-[16px] flex items-center justify-center rounded-xl`}
+    >
+      {title}
+    </Link>
+  ) : (
+    <button
+      className={`${bgColor ? bgColor : "bg-main"} ${textColor ? textColor : "text-white"} w-[320px] h-[48px] font-semibold text-[16px] flex items-center justify-center rounded-xl`}
+    >
+      {title}
+    </button>
+  );
+}
