0638a6bbe04b8dfd681120f5a561291cc186140e
kangmin01 <minheek99@gmail.com>
2024-06-29 03:05:24 +0900

    feat: Add terms and conditions to profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -10,6 +10,7 @@ import CheckIcon from "../components/ui/icons/CheckIcon";
 import { useMessage } from "../context/MessageContext";
 import { Snackbar } from "../components/SnackBar";
 import axiosInstance from "../api/axiosInstance";
+import useDeviceSize from "../hooks/useDeviceSize";
 
 export default function Profile() {
   const token = localStorage.getItem("access_token");
@@ -19,6 +20,8 @@ export default function Profile() {
   const { logout } = useAuthContext();
 
   const { state, clearMessage } = useMessage();
+
+  const { isDesktop } = useDeviceSize();
 
   useEffect(() => {
     if (state.message) {
@@ -61,15 +64,18 @@ export default function Profile() {
 
   return (
     <section className="bg-white max-w-[360px] mx-auto h-dvh relative">
-      <Header title="내 정보" />
-      <div className="pt-[24px] px-[20px]">
-        <div className="border-solid border-b-[1px] border-gray0 w-[320px] h-[50px] flex items-center justify-between">
+      <Header title="프로필" />
+      <div className="pt-[24px] px-[20px] mb-[24px]">
+        <span className="block pl-[16px] font-semibold text-[16px] text-gray4">
+          내 정보
+        </span>
+        <div className="mt-[16px] p-[16px] border-solid border-b-[1px] border-gray0 w-[320px] h-[50px] flex items-center justify-between">
           <span className="text-[14px] font-medium text-gray2">이름</span>
           <span className="text-[16px] font-normal text-gray4">
             {info?.user_name}
           </span>
         </div>
-        <div className="border-solid border-b-[1px] border-gray0 w-[320px] h-[50px] flex items-center justify-between">
+        <div className="p-[16px] border-solid border-b-[1px] border-gray0 w-[320px] h-[50px] flex items-center justify-between">
           <span className="text-[14px] font-medium text-gray2">이메일</span>
           <span className="text-[16px] font-normal text-gray4">
             {info?.email}
@@ -77,7 +83,7 @@ export default function Profile() {
         </div>
         <Link
           to="/profile/change-password"
-          className="border-solid border-b-[1px] border-gray0 w-[320px] h-[50px] flex items-center justify-between"
+          className="p-[16px] border-solid border-b-[1px] border-gray0 w-[320px] h-[50px] flex items-center justify-between"
         >
           <span className="text-[14px] font-medium text-gray2">
             비밀번호 변경
@@ -87,7 +93,36 @@ export default function Profile() {
           </span>
         </Link>
       </div>
-      <div className="fixed bottom-0 w-[360px] h-[56px] text-center">
+
+      <div className="px-[20px]">
+        <span className="block pl-[16px] font-semibold text-[16px] text-gray4">
+          약관
+        </span>
+        <Link
+          to="/"
+          className="mt-[16px] p-[16px] border-solid border-b-[1px] border-gray0 w-[320px] h-[50px] flex items-center justify-between"
+        >
+          <span className="text-[14px] font-medium text-gray2">이용약관</span>
+          <span className="text-[16px] font-normal text-gray4">
+            <RightChevronThin />
+          </span>
+        </Link>
+        <Link
+          to="/"
+          className="p-[16px] border-solid border-b-[1px] border-gray0 w-[320px] h-[50px] flex items-center justify-between"
+        >
+          <span className="text-[14px] font-medium text-gray2">
+            개인정보처리방침
+          </span>
+          <span className="text-[16px] font-normal text-gray4">
+            <RightChevronThin />
+          </span>
+        </Link>
+      </div>
+
+      <div
+        className={`fixed w-[360px] h-[56px] text-center ${isDesktop ? "top-[834px]" : "bottom-0"}`}
+      >
         <div className="font-normal text-[14px] text-gray2">
           <span onClick={handleLogout} className="cursor-pointer">
             로그아웃
