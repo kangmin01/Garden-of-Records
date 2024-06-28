fc07aeb052959be04c0b122ce2783863edb8bc7a
kangmin01 <minheek99@gmail.com>
2024-06-29 02:38:11 +0900

    fix: Adjust floating button position

--- src/components/ui/FloatingButton.tsx
+++ src/components/ui/FloatingButton.tsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import PlusIcon from "./icons/PlusIcon";
 import { useEffect, useState } from "react";
+import useDeviceSize from "../../hooks/useDeviceSize";
 
 export default function FloatingButton() {
   const [displayHeight, setDisplayHeight] = useState(window.innerHeight);
   const [ready, setReady] = useState(false);
   // console.log(displayHeight);
+
+  const { isDesktop } = useDeviceSize();
 
   useEffect(() => {
     const handleResize = () => {
@@ -25,17 +28,17 @@ export default function FloatingButton() {
     <>
       {ready && (
         <div
-          className={`w-[360px] fixed h-10 bottom-0`}
+          className={`w-[360px] fixed ${isDesktop ? "h-[800px] top-[90px]" : "h-10 bottom-0"}`}
           // style={{ top: `${displayHeight - 1}px` }}
         >
-          <div className="w-[360px] mx-auto relative h-10">
-            <Link
-              to="/record/add"
-              className="absolute w-[52px] h-[52px] bottom-[34px] right-[30px] flex justify-center items-center bg-main text-white rounded-full shadow-shadowFloatingButton"
-            >
-              <PlusIcon />
-            </Link>
-          </div>
+          {/* <div className="w-[360px] mx-auto relative h-10"> */}
+          <Link
+            to="/record/add"
+            className="absolute w-[52px] h-[52px] bottom-[34px] right-[30px] flex justify-center items-center bg-main text-white rounded-full shadow-shadowFloatingButton"
+          >
+            <PlusIcon />
+          </Link>
+          {/* </div> */}
         </div>
       )}
     </>
